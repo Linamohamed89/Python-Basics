332f9493fb3aecea547937efb2b2f478d952aa3c
Linamohamed89 <linaaloqaily1989@gmail.com>
2023-06-14 20:46:11 +0200

    add python variables

---
+++ 1. variables.py
@@ -0,0 +1,40 @@
+
+#type 
+x=5
+s='Lina Mohammed'
+print (type(x))
+print (type(s))
+
+
+
+#str
+
+x='5'
+print(type(x))
+
+
+#float
+
+h=5.0
+print (type(h))
+
+#list
+
+g=[1,2,3,4,5,True]
+print (type(g))
+
+#tuple
+
+p=(1,2,3,False)
+print (type(p))
+
+
+
+#tuple - str
+
+p=('welcome') #str
+j=('welcome',)#tuple
+
+print (type(p))
+print (type(j))
+
