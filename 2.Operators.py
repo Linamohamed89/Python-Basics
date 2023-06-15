4e2de5b4f3cb5825c92386e226b97a9ec9f0ec79
Linamohamed89 <linaaloqaily1989@gmail.com>
2023-06-15 18:15:53 +0200

    add arithmetic operators

---
+++ 2.Operators.py
@@ -0,0 +1,33 @@
+ #operators
+
+#1 aeithmetic op
+# +
+A=5+5
+print(A)
+
+
+# -
+B=10-4
+print(B)
+#6
+
+
+# *
+C=2*8
+print(C)
+#16
+
+
+# /
+D=9/3
+print(D)
+#3.0 /=float
+I=9/4
+print(I)
+#2.25
+
+# //
+F=9//4
+print(F)
+#2 //=int
+2 py file
