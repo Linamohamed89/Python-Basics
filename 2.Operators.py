6a554c1e6fe1476c393fda66b796a1549c8b7282
Linamohamed89 <linaaloqaily1989@gmail.com>
2023-06-15 19:39:51 +0200

    add Nested if

--- 2.Operators.py
+++ 2.Operators.py
@@ -1,6 +1,6 @@
  #operators
 
-#1 aeithmetic op
+#1 Arithmetic op
 # +
 A=5+5
 print(A)
@@ -50,7 +50,7 @@ print(x==10)
 
 ###########################################
 
-# Assignment op
+#2 Assignment op
 
 # +=
 x=10
@@ -69,7 +69,47 @@ print(x)
 
 ###########################################
 
-#Connditions op
+#3 Conditions op
+
+# if else example
+
+x=500
+if x > 600:
+    print('x is beigger than 600')
+else:
+    print('x is smaller than 600')
+
+x=600
+if x > 500:
+    print('Hello')
+elif x>400:
+    print('x > 400')
+else:
+    print('-------')
+
+    
+
+###########################################
+
+#4 Nested if
+
+# Nested if Ex
+
+x=100
+if x < 200:
+    print('x is less than 200')
+
+    if x == 150:
+        print('x=150')
+    elif x== 50:
+        print('x=50')
+elif x<50:
+    print('x is less than 50')
+    
+    
+
+
+
 
 
 
