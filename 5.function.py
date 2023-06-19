f886f26b95161740da4211c47afdd5d098d4f8b0
Linamohamed89 <linaaloqaily1989@gmail.com>
2023-06-19 22:06:21 +0200

    add mysum

--- 5.function.py
+++ 5.function.py
@@ -19,3 +19,29 @@ False
 mysum(5+10)
 mysum(15+100)
 '''
+
+'''
+##
+5,10
+#1---->100
+
+
+def divdedby(x,y):
+    for n in range(1,101):
+        if n%x==0 and n%y==0:
+            print(n)
+
+divdedby(3,5)
+'''
+
+def mysum1(x,y):
+    return x+y
+
+
+def mysum2(x,y):
+    print(x+y)
+
+b=mysum1(5,10)
+print(b)
+n=mysum2(20,30)
+print(n)
