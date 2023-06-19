340ad3026771e09ba249d731ad935ac106ebffbf
Linamohamed89 <linaaloqaily1989@gmail.com>
2023-06-19 18:05:40 +0200

    add **

--- python input.py
+++ python input.py
@@ -27,3 +27,30 @@ print('--------------')
 for x in range(start,end+1):
     print(x,'\t',x*x)
 '''
+
+
+## ********** x 5 
+start= int(input('Enter Start : '))
+end=int(input('Enter End : '))
+
+for x in range (start):
+        print('*'*end)
+
+## ****-->>50x
+start= int(input('Enter Start : '))
+end=int(input('Enter End : '))
+
+for x in range (start):
+    for y in range (end):
+        print('*',end='')
+
+## *** 10x
+## *** 10x -->>5x
+
+start= int(input('Enter Start : '))
+end=int(input('Enter End : '))
+
+for x in range (start):
+    for y in range (end):
+        print('*',end='')
+    print('')
