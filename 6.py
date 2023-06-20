8eb71caaa8a42ce738ac049d4f9ad7fc108de727
Linamohamed89 <linaaloqaily1989@gmail.com>
2023-06-20 23:41:15 +0200

    add text[-] and list

--- 6.py
+++ 6.py
@@ -1,6 +1,6 @@
 
 
-####
+#### string
 
 s='welcome'
 f="welcome"
@@ -41,3 +41,21 @@ print(text.replace('is','was')) #change word
 print(text.split(','))
 
 print(text.split(' '))
+
+
+print(text[0]) #first litter
+print(text[-1])#last letter
+print(text[-2])#befor last letter
+print(text[0:6])#letter 1 to 6
+print(text[:6])# 0 to 6
+print(text[6:])#6 to end
+
+
+# List
+
+l=[1,2,3,4,True,'welcome']
+
+print(l[0]) #1
+print(l[-1]) #'welcome'
+l[0]=100
+print(l[0]) #l=[100,2,3,4,True,'welcome'] jest in list
