9bfb0e47ff42c4d8df69196a6cc1f73384607a89
Linamohamed89 <linaaloqaily1989@gmail.com>
2023-06-22 14:45:07 +0200

    add new function

--- 7.py
+++ 7.py
@@ -29,3 +29,68 @@ c1.sum(2,3)
 
 c2=calculator()
 c2.sum(20,13)
+
+
+print('--------------------------------')
+
+
+class calculator:
+    def sum (self,x,y):
+        print(x+y)
+ 
+
+
+c1=calculator()
+c1.sum(2,3)
+
+c2=calculator()
+c2.sum(20,13)
+
+print('--------------------------------')
+
+class calculator:
+    def mul (self,x,y):
+        print(x*y)
+ 
+
+
+c1=calculator()
+c1.mul(2,3)
+
+c2=calculator()
+c2.mul(20,13)
+
+
+print('--------------------------------')
+
+'''
+def __init__(self,name):
+    print(f'welcome{name}')
+
+
+c1=calculator('mahmoud')
+
+'''
+print('--------------------------------')
+
+class calculator:
+    def sum(self,x,y):
+        print(x+y)
+
+    def mul(self,x,y):
+        print(x*y)
+
+
+class  SciCalc:
+    def sum(self,x,y):
+        print(x+y)
+
+    def mul(self,x,y):
+        print(x*y)
+
+    def power(self,x,y):
+        print(x**y)
+
+
+
+
