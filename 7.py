ee62b6700a2b05a77036d3fa266bd1dd6d35b83c
Linamohamed89 <linaaloqaily1989@gmail.com>
2023-06-22 08:33:05 +0200

    add new py file

---
+++ 7.py
@@ -0,0 +1,31 @@
+
+
+
+###
+
+'''
+class calculator:
+    def sum (x,y):
+    print(x+y)
+
+
+c1=calculator()
+c1.sum(2,3)
+
+c2=calculator()
+c2.sum(20,13)
+'''
+
+#True
+
+class calculator:
+    def sum (mystro,x,y):
+        print(x+y)
+ 
+
+
+c1=calculator()
+c1.sum(2,3)
+
+c2=calculator()
+c2.sum(20,13)
