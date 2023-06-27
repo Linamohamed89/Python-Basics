24f8d2d992a02855583ba467cba9e502e49b5ae6
Linamohamed89 <linaaloqaily1989@gmail.com>
2023-06-27 22:46:20 +0200

    add Escape Double Quotes

--- 9.py
+++ 9.py
@@ -11,11 +11,17 @@ print('Hello\bWorld')
 print('Hello\
       I Love\
       python')
-
+#Hello      I Love      python
 
 #Escape Back Slash
 print('I Love Back Slash \\')
-
+#I Love Back Slash \
 
 #Escape Single Quote
 print('I Love Single Quote \'Test\'')
+#I Love Single Quote 'Test'
+
+
+#Escape Double Quotes
+print('I Love Double Quotes \"Test\"')
+#I Love Single Quote "Test"
